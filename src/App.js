701d6deb6f6b28ca848569a0987fd78ec6e77f81
Nyabongo E.T <nyabongoedgar@gmail.com>
2019-10-03 16:37:31 +0300

    tic tac toe version one

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      winner: undefined,
+    };
+    this.gameState = {
+        turn: 'X',
+        gameLocked: false,
+        gameEnded: false,
+       board: Array(9).fill(''),
+       totalMoves: 0
+    }
+  }
+
+  clicked(box) {
+    if(this.gameState.gameEnded || this.gameState.gameLocked) return;
+
+    if(this.gameState.board[box.dataset.square] === '') {
+      this.gameState.board[box.dataset.square] = this.gameState.turn;
+      box.innerText = this.gameState.turn;
+      
+      this.gameState.turn = this.gameState.turn === 'X' ? 'O' : 'X';
+      
+      this.gameState.totalMoves++;
+    
+
+    var result = this.checkWinner();
+
+    if(result === 'X') {
+      this.gameState.gameEnded = true;
+      this.setState({
+        winner: 'X',
+        winnerLine: 'Match won by X'
+      });
+    } else if(result === 'O') {
+      this.gameState.gameEnded = true;
+      this.setState({
+        winner: 'O',
+        winnerLine: 'Match won by O'
+      });
+    } else if(result === 'draw') {
+      this.gameState.gameEnded = true;
+      this.setState({
+        winner: 'draw',
+        winnerLine: 'Match is drawn'
+      })
+    }
+    
+    if(this.gameState.turn === 'O' && !this.gameState.gameEnded) {
+      this.gameState.gameLocked = true;
+      setTimeout(()=> {
+        do {
+          //we need logic here to know where x is
+          // if x is in 0 fill 3, if in 1 fill 4, if in 
+          var random = Math.floor(Math.random()*9);
+        } while(this.gameState.board[random] !== '');
+        this.gameState.gameLocked = false;
+        this.clicked(document.querySelectorAll('.square')[random]);
+      }, 1000);
+
+    }
+
+  }
+
+  checkWinner(){
+    var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    var board = this.gameState.board;
+    for(let i=0;i<moves.length;i++) {
+      if(board[moves[i][0]] === board[moves[i][1]] && board[moves[i][1]] === board[moves[i][2]])
+          return board[moves[i][0]];
+    }
+
+    if(this.gameState.totalMoves === 9) {
+      return 'draw';
+    }
+  }
+
+  render(){
+    return (
+      <div id="game">
+          <div id="status">{this.state.winnerLine}</div>
+          <div id="head">
+              World's best tic tac toe AI
+          </div>
+          <div id="board" onClick={(e)=>this.clicked(e.target)}>
+              <div className="square" data-square="0"></div>
+              <div className="square" data-square="1"></div>
+              <div className="square" data-square="2"></div>
+              <div className="square" data-square="3"></div>
+              <div className="square" data-square="4"></div>
+              <div className="square" data-square="5"></div>
+              <div className="square" data-square="6"></div>
+              <div className="square" data-square="7"></div>
+              <div className="square" data-square="8"></div>
+          </div>
+      </div>      
+    );
+  }
 }
+
 
 export default App;
